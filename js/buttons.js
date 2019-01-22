5a1dbf2e9ec7becb016f1752ed4cdfb0e582fe48
Sonic Keung <skeung3@my.bcit.ca>
2019-01-22 14:42:24 -0800

    lab2

--- js/buttons.js
+++ js/buttons.js
@@ -3,21 +3,19 @@ function getLineSeparator() {
 	document.body.appendChild(br);
 }
 
-function displayButton() {
-	for (var i = 0; i < 26; i++) {
-		let btn = document.createElement("BUTTON");
-		btn.innerHTML = (i + 10).toString(36).toUpperCase() + " ";
-		btn.style.listStyle = "none";
-		btn.style.display = "inline";
-		document.body.appendChild(btn);
-		
-		btn.onclick = function alphabetAlert(e) {
-			alert(e.target.innerHTML);
-		}
-		
-		if (i === 12) {
-			getLineSeparator();
-		}
+for (var i = 0; i < 26; i++) {
+	let btn = document.createElement("BUTTON");
+	btn.innerHTML = (i + 10).toString(36).toUpperCase() + " ";
+	btn.style.listStyle = "none";
+	btn.style.display = "inline";
+	document.body.appendChild(btn);
+	
+	btn.onclick = function alphabetAlert(e) {
+		alert(e.target.innerHTML);
 	}
 	
+	if (i === 12) {
+		getLineSeparator();
+	}
 }
+
